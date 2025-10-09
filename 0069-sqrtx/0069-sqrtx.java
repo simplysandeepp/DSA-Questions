ab8be1b918d23e729b35aa79cde3794d09f7b14d
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-10-10 00:10:17 +0530

    Time: 1 ms (85.1%), Space: 41.3 MB (21.53%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x <= 1) return x; // Base cases
+
+        int i = 1, j = x;
+        int mid = i + (j - i) / 2;
+
+        while (i <= j) {
+            if (mid == x / mid) return mid;
+            else if (mid > x / mid)
+                j = mid - 1;
+            else
+                i = mid + 1;
+
+            mid = i + (j - i) / 2;
+        }
+        return j; // j will be floor(sqrt(x))
+    }
+}
