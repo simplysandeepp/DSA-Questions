ee3c9314c834f8e251f48bfe66996efbdf79bbc6
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-10-20 13:10:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -0,0 +1,17 @@
+class NumArray {
+    private int[] prefix;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        prefix = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+    }
+    
+    public int sumRange(int left int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+}
