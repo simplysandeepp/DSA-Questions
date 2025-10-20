645dfb641ef469ec514bd037481e851277afc325
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-10-20 23:39:10 +0530

    Time: 11 ms (74.13%), Space: 46 MB (20.33%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,17 +1,24 @@
+
 class Solution {
     public String predictPartyVictory(String senate) {
         int n = senate.length();
-        Queue<Integer> radiant = new LinkedList<>();
-        Queue<Integer> dire = new LinkedList<>();
+        Deque<Integer> radiant = new LinkedList<>();
+        Deque<Integer> dire = new LinkedList<>();
         for (int i = 0; i < n; i++) {
-            if (senate.charAt(i) == 'R') radiant.offer(i);
-            else dire.offer(i);
+            if (senate.charAt(i) == 'R') {
+                radiant.offer(i);
+            } else {
+                dire.offer(i);
+            }
         }
         while (!radiant.isEmpty() && !dire.isEmpty()) {
-            int r = radiant.poll();
-            int d = dire.poll();
-            if (r < d) radiant.offer(r + n);
-            else dire.offer(d + n);
+            int r_idx = radiant.poll();
+            int d_idx = dire.poll();
+            if (r_idx < d_idx) {
+                radiant.offer(r_idx + n);
+            } else {
+                dire.offer(d_idx + n);
+            }
         }
         return radiant.isEmpty() ? "Dire" : "Radiant";
     }
