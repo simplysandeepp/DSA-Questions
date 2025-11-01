72e94457478317bbb4b1cb17aef0c6e688aaa23e
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-11-01 16:46:15 +0530

    Time: 0 ms (100%), Space: 42.1 MB (5.67%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1||n==0)
+        {
+            return 1;
+        }
+        int[] steps=new int[n+1];
+        steps[0]=steps[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            steps[i]=steps[i-1]+steps[i-2];
+        }
+        return steps[n];
+    }
+}
