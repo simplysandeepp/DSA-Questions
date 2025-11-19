79c3eceb04fa115a1414875f826bc7715e647eab
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-11-19 09:37:51 +0530

    Time: 0 ms (100%), Space: 44 MB (21.43%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -4,8 +4,8 @@ class Solution {
             return head;
         }
         ListNode newNode = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
+        head.next.next = head; // yaha par we r making the node link to opp. initially 4 -> 5 but ab 4 <- 5 
+        head.next = null; // prev link break kar rhe hai null connect krke
         return newNode;
     }
     
