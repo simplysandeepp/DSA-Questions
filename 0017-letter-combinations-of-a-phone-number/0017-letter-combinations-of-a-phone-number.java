af5a33d9cf8a9aff8f8a29195b7a655200aac0e3
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-11-20 12:15:04 +0530

    Time: 3 ms (34.74%), Space: 49.2 MB (6.06%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,29 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    private static final String[] PHONE_MAP = {
+    String[] map = {
         "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
     };
-    
-    public List<String> letterCombinations(String digits) {
-        List<String> combinations = new ArrayList<>();
-        if (digits.isEmpty()) return combinations;
-        backtrack(0, digits, new StringBuilder(), combinations);
-        return combinations;
+    public List<String> letterCombinations(String digits){
+        List <String> ans = new ArrayList<>();
+        helper(digits, 0, "", ans);
+        return ans;
     }
-    
-    private void backtrack(int index, String digits, StringBuilder path, List<String> combinations) {
-        if (index == digits.length()) {
-            combinations.add(path.toString());
+    public void helper(String digits, int index, String curr, List<String> ans){
+        if (index == digits.length()){
+            ans.add(curr);
             return;
         }
-        
-        String possibleLetters = PHONE_MAP[digits.charAt(index) - '0'];
-        for (char letter : possibleLetters.toCharArray()) {
-            path.append(letter);
-            backtrack(index + 1, digits, path, combinations);
-            path.deleteCharAt(path.length() - 1); // Backtrack
+        for(char c : map[digits.charAt(index) - '0'].toCharArray()) {
+            helper(digits, index+1, curr + c, ans);
         }
+
+
     }
 }
