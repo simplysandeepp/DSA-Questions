17bdc1ce604415dacf42e513ee985291e2ad973f
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-12-02 10:33:16 +0530

    Time: 0 ms (100%), Space: 42.6 MB (35.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,23 @@
 class Solution {
+    int[] dp;
+
     public int rob(int[] nums) {
         int n = nums.length;
+        dp = new int[n];
+        
+        Arrays.fill(dp, -1);
 
-        if (n == 1) {
-            return nums[0];
-        }
+        return solve(nums, n - 1);
+    }
 
-        int[] dp = new int[n];
+    private int solve(int[] nums, int i) {
 
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        if (i < 0) return 0;
+        
+        if (dp[i] != -1) return dp[i];
+        
+        dp[i] = Math.max(solve(nums, i - 1), nums[i] + solve(nums, i - 2));
 
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
-        }
-
-        return dp[n - 1];        
+        return dp[i];
     }
 }
