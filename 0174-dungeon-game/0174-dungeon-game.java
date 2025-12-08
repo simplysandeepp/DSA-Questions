19a6113560a97580d77e30fbeb2927df061bc6a9
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-12-08 11:31:21 +0530

    Time: 1 ms (69.6%), Space: 44.9 MB (91.08%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,24 +1,65 @@
 class Solution {
-    public int calculateMinimumHP(int[][] dungeon) {
-        int m = dungeon.length;
-        int n = dungeon[0].length;
-        
-        int[][] dp = new int[m + 1][n + 1];
-        
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                dp[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        dp[m][n-1] = 1;
-        dp[m-1][n] = 1;
+    int m, n;
+    int[][] dp;
 
-        for (int i = m-1; i >= 0; i--) {
-            for (int j = n-1; j >= 0; j--) {
-                int reqH = Math.min(dp[i + 1][j], dp[i][j + 1]);
-                dp[i][j] = Math.max(1, reqH - dungeon[i][j]);
-            }
+    public int calculateMinimumHP(int[][] power) {
+        m = power.length;
+        n = power[0].length;
+        dp = new int[m][n];
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                dp[i][j] = -1;
+
+        return solve(0, 0, power);
+    }
+
+    int solve(int i, int j, int[][] power) {
+        if (i >= m || j >= n) return Integer.MAX_VALUE;
+
+        if (i == m - 1 && j == n - 1) {
+            return power[i][j] >= 0 ? 1 : 1 - power[i][j];
         }
-        return dp[0][0];
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int down = solve(i + 1, j, power);
+        int right = solve(i, j + 1, power);
+
+        int need = Math.min(down, right) - power[i][j];
+        dp[i][j] = need <= 0 ? 1 : need;
+
+        return dp[i][j];
     }
 }
+
+
+
+// Recursive Approach !!
+
+/*
+class Solution {
+    int m, n;
+
+    public int calculateMinimumHP(int[][] power) {
+        m = power.length;
+        n = power[0].length;
+        return solve(0, 0, power);
+    }
+
+    int solve(int i, int j, int[][] power) {
+        if (i >= m || j >= n) 
+        return Integer.MAX_VALUE;
+
+        if (i == m - 1 && j == n - 1) {
+            return power[i][j] >= 0 ? 1 : 1 - power[i][j];
+        }
+
+        int down = solve(i + 1, j, power);
+        int right = solve(i, j + 1, power);
+
+        int need = Math.min(down, right) - power[i][j];
+        return need <= 0 ? 1 : need;
+    }
+}
+*/
