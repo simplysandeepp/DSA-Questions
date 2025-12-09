6153f7206b098ab509be0baf50970a22c2227a61
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-12-09 11:14:22 +0530

    Time: 17 ms (46.72%), Space: 46.9 MB (17.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,34 +1,34 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
+
         int[][] dp = new int[n][amount + 1];
 
         for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+            Arrays.fill(dp[i], (int)1e9);
         }
 
-        int ans = solveRec(n - 1, amount, coins, dp);
-        return ans >= 1e9 ? -1 : ans;
-    }
-
-    private int solveRec(int idx, int amount, int[] coins, int[][] dp) {
-        if (idx == 0) {
-            if (amount % coins[0] == 0) {
-                return amount / coins[0];
+        for (int j = 0; j <= amount; j++) {
+            if (j % coins[0] == 0) {
+                dp[0][j] = j / coins[0];
             }
-            return (int) 1e9; 
         }
 
-        if (dp[idx][amount] != -1) 
-            return dp[idx][amount];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
 
-        int notTake = solveRec(idx - 1, amount, coins, dp);
+                int notTake = dp[i - 1][j];  
+                int take = (int)1e9;
 
-        int take = (int) 1e9;
-        if (coins[idx] <= amount) {
-            take = 1 + solveRec(idx, amount - coins[idx], coins, dp);
+                if (coins[i] <= j) {
+                    take = 1 + dp[i][j - coins[i]]; 
+                }
+
+                dp[i][j] = Math.min(take, notTake);
+            }
         }
 
-        return dp[idx][amount] = Math.min(take, notTake);
+        int ans = dp[n - 1][amount];
+        return ans >= 1e9 ? -1 : ans;
     }
 }
