5a1e57a0937e267483451d9b4a60e6cd4ce66147
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-12-22 11:55:05 +0530

    Time: 86 ms (20.73%), Space: 237.2 MB (6.86%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,3 +1,23 @@
+// RECURSION !!
+
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         return solve(0, -1, nums);
+//     }
+//     public int solve(int curr, int prev, int[] nums) {
+//         if(curr == nums.length) {
+//             return 0;
+//         }
+//         int notTake = solve(curr+1 , prev, nums);
+//         int take =0;
+//         if(prev == -1 || nums[curr] > nums[prev]) {
+//             take = 1 + solve(curr+1, curr, nums);
+//         }
+//         return Math.max(take, notTake);
+//     }
+// }
+
+// MEMOIZATION
 // class Solution {
 //     public int lengthOfLIS(int[] nums) {
 //         int n = nums.length;
@@ -20,6 +40,33 @@
 //         return dp[curr][prev+1];
 //     }
 // }
+
+
+
+// TABULATION
+
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         int[][] dp = new int[n + 1][n + 1];
+
+//         for (int curr = n - 1; curr >= 0; curr--) {
+//             for (int prev = curr - 1; prev >= -1; prev--) {
+//                 int notTake = dp[curr + 1][prev + 1];
+//                 int take = 0;
+//                 if (prev == -1 || nums[curr] > nums[prev]) {
+//                     take = 1 + dp[curr + 1][curr + 1];
+//                 }
+//                 dp[curr][prev + 1] = Math.max(take, notTake);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// }
+
+
+// SPACE OPTIMIZATION - 1 D ARRAY
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
@@ -27,18 +74,14 @@ class Solution {
 
         for (int curr = n - 1; curr >= 0; curr--) {
             for (int prev = curr - 1; prev >= -1; prev--) {
-
                 int notTake = dp[curr + 1][prev + 1];
                 int take = 0;
-
                 if (prev == -1 || nums[curr] > nums[prev]) {
                     take = 1 + dp[curr + 1][curr + 1];
                 }
-
                 dp[curr][prev + 1] = Math.max(take, notTake);
             }
         }
-
         return dp[0][0];
     }
 }
