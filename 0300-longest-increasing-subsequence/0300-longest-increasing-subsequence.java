305868ba16fdf24e0008f3fe1cf5651857adec66
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-12-22 15:42:07 +0530

    Time: 83 ms (21.7%), Space: 237.2 MB (9.58%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,16 +3,16 @@ class Solution {
         int n = nums.length;
         int[][] dp = new int[n + 1][n + 1];
 
-        // for (int curr = n - 1; curr >= 0; curr--) {
-        //     for (int prev = curr - 1; prev >= -1; prev--) {
-        //         int notTake = dp[curr + 1][prev + 1];
-        //         int take = 0;
-        //         if (prev == -1 || nums[curr] > nums[prev]) {
-        //             take = 1 + dp[curr + 1][curr + 1];
-        //         }
-        //         dp[curr][prev + 1] = Math.max(take, notTake);
-        //     }
-        // }
+        for (int curr = n - 1; curr >= 0; curr--) {
+            for (int prev = curr - 1; prev >= -1; prev--) {
+                int notTake = dp[curr + 1][prev + 1];
+                int take = 0;
+                if (prev == -1 || nums[curr] > nums[prev]) {
+                    take = 1 + dp[curr + 1][curr + 1];
+                }
+                dp[curr][prev + 1] = Math.max(take, notTake);
+            }
+        }
         return dp[0][0];
     }
 }
