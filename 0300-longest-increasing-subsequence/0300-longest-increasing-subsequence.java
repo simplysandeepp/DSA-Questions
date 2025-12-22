e70c770c5117729be3ad2529401f51d4a5e4551e
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-12-22 15:40:37 +0530

    Time: 85 ms (21.07%), Space: 237.5 MB (6.86%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -41,32 +41,7 @@
 //     }
 // }
 
-
-
-// TABULATION
-
-// class Solution {
-//     public int lengthOfLIS(int[] nums) {
-//         int n = nums.length;
-//         int[][] dp = new int[n + 1][n + 1];
-
-//         for (int curr = n - 1; curr >= 0; curr--) {
-//             for (int prev = curr - 1; prev >= -1; prev--) {
-//                 int notTake = dp[curr + 1][prev + 1];
-//                 int take = 0;
-//                 if (prev == -1 || nums[curr] > nums[prev]) {
-//                     take = 1 + dp[curr + 1][curr + 1];
-//                 }
-//                 dp[curr][prev + 1] = Math.max(take, notTake);
-//             }
-//         }
-//         return dp[0][0];
-//     }
-// }
-
-
-// SPACE OPTIMIZATION - 1 D ARRAY
-
+//      TABULATION
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
