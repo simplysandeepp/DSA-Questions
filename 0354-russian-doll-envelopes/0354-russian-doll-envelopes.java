5f1cb3b30df32e71b16ca22c4c0aa34a86c354f8
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-12-23 10:44:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes == null || envelopes.length == 0) {
+            return 0;
+        }
+        Arrays.sort(envelopes, (a, b) -> {
+            if (a[0] != b[0]) {
+                return a[0] - b[0];   
+            }
+            if (a[1] != b[1]) {
+                return b[1] - a[1];   
+            }
+            return 0;
+        });
+        int n= envelopes.length;
+        int[] dp = new int[n];
+        int len = 0;
+
+        for (int i = 0; i < n; i++) {
+            int height = envelopes[i][1];
+
+            int index = Arrays.binarySearch(dp, 0, len, height);
+            if (index < 0) {
+                index = -(index + 1);
+            }
+            dp[index] = height;
+
+            if (index == len) {
+                len++;
+            }
+        }
+
+        return len'
+    }
+}
