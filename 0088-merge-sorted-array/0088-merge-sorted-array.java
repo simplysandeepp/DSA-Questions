55c1e8faad0c2a7397c7639acdcb902ab2172d23
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-12-27 00:17:26 +0530

    Time: 0 ms (100%), Space: 43.9 MB (34.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,10 +1,9 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;       // last element in nums1’s initial part
-        int j = n - 1;       // last element in nums2
-        int k = m + n - 1;   // last position in nums1
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;   
 
-        // Merge from back
         while (i >= 0 && j >= 0) {
             if (nums1[i] > nums2[j]) {
                 nums1[k--] = nums1[i--];
@@ -13,7 +12,6 @@ class Solution {
             }
         }
 
-        // Copy remaining nums2 elements if any
         while (j >= 0) {
             nums1[k--] = nums2[j--];
         }
