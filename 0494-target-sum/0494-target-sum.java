4e1a497a1d0d5bb7bd006dfb7eda2763e28c2f30
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-01-04 00:18:52 +0530

    Time: 9 ms (55.81%), Space: 47.1 MB (16.73%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,23 @@
+public class Solution {
+
+    public int findTargetSumWays(int[] nums, int target) {
+        int totalSum = Arrays.stream(nums).sum();
+        int[][] dp = new int[nums.length][2 * totalSum + 1];
+
+        dp[0][nums[0] + totalSum] = 1;
+        dp[0][-nums[0] + totalSum] += 1;
+
+        for (int index = 1; index < nums.length; index++) {
+            for (int sum = -totalSum; sum <= totalSum; sum++) {
+                if (dp[index - 1][sum + totalSum] > 0) {
+                    dp[index][sum + nums[index] + totalSum] += dp[index -
+                        1][sum + totalSum];
+                    dp[index][sum - nums[index] + totalSum] += dp[index -
+                        1][sum + totalSum];
+                }
+            }
+        }
+
+        return Math.abs(target) > totalSum ? 0 : dp[nums.length - 1][target + totalSum];
+    }
+}
