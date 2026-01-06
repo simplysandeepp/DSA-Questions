0d00c8067887e06b542a8fb2c290dc40e7287492
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-01-06 11:34:14 +0530

    Time: 0 ms (100%), Space: 43 MB (14.34%) - LeetHub

--- 0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.java
+++ 0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.java
@@ -1,11 +1,18 @@
 class Solution {
+
+    int[][] dp;
+
     public int atMostNGivenDigitSet(String[] digits, int n) {
         int mask = 0;
         for (int i = 0; i < digits.length; i++) {
             int digit = Integer.parseInt(digits[i]);
-            mask = mask | (1 << digit);  
+            mask = mask | (1 << digit);
         }
         String s = String.valueOf(n);
+        dp = new int[s.length()][2];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][0] = dp[i][1] = -1;
+        }
         int res = 0;
         for (int i = 1; i < s.length(); i++) {
             res += Math.pow(digits.length, i);
@@ -15,21 +22,23 @@ class Solution {
     }
 
     public int solve(String s, int idx, int tight, int mask) {
-        if (idx == s.length()) {   
+        if (idx == s.length()) {
             return 1;
+        }
+        if (dp[idx][tight] != -1) {
+            return dp[idx][tight];
         }
         int lb = 0;
         int ub = (tight == 1) ? s.charAt(idx) - '0' : 9;
         int res = 0;
-        
-        for (int digit = lb; digit <= ub; digit++) {  
+        for (int digit = lb; digit <= ub; digit++) {
             int newTight = (tight == 1 && digit == ub) ? 1 : 0;
-            int isDigitUsed = (mask >> digit) & 1;    
-            if (isDigitUsed == 0) {
-                continue;
-            }
+
+            int isDigitUsed = (mask >> digit) & 1;
+            if (isDigitUsed == 0) continue;
+
             res += solve(s, idx + 1, newTight, mask);
         }
-        return res;
+        return dp[idx][tight] = res;
     }
 }
