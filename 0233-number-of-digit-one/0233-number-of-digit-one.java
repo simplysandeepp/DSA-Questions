594c45fe6f750e1d9fe28e3f7b308f25bf86c111
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-01-06 09:55:50 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.22%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.java
+++ 0233-number-of-digit-one/0233-number-of-digit-one.java
@@ -1,31 +1,46 @@
 class Solution {
-    Integer[][] dp;
     String s;
+    long[][][] dp; 
     public int countDigitOne(int n) {
         s = String.valueOf(n);
         int len = s.length();
-        dp = new Integer[len][2];
-        return solve(0, 1);
-    }
-    private int solve(int idx, int tight) {
-        if (idx == s.length()) {
-            return 0;
-        }
-        if (dp[idx][tight] != null) {
-            return dp[idx][tight];
-        }
-        int ub = (tight == 1) ? s.charAt(idx) - '0' : 9;
-        int res = 0;
-
-        for (int digit = 0; digit <= ub; digit++) {
-            int newTight = (tight == 1 && digit == ub) ? 1 : 0;
-            if (digit == 1) {
-                res += 1 + solve(idx + 1, newTight);
-            } else {
-                res += solve(idx + 1, newTight);
+        dp = new long[len][2][2];
+        for (int i = 0; i < len; i++) {
+            for (int t = 0; t < 2; t++) {
+                dp[i][t][0] = -1;
             }
         }
 
-        return dp[idx][tight] = res;
+        return (int) solve(0, 1)[0];
+    }
+
+    private long[] solve(int idx, int tight) {
+        if (idx == s.length()) {
+            return new long[]{0, 1}; 
+        }
+
+        if (dp[idx][tight][0] != -1) {
+            return dp[idx][tight];
+        }
+
+        int ub = (tight == 1) ? s.charAt(idx) - '0' : 9;
+        long countOnes = 0;
+        long countNums = 0;
+
+        for (int digit = 0; digit <= ub; digit++) {
+            int newTight = (tight == 1 && digit == ub) ? 1 : 0;
+            long[] next = solve(idx + 1, newTight);
+
+            countNums += next[1];
+            countOnes += next[0];
+
+            if (digit == 1) {
+                countOnes += next[1]; 
+            }
+        }
+
+        dp[idx][tight][0] = countOnes;
+        dp[idx][tight][1] = countNums;
+        return dp[idx][tight];
     }
 }
