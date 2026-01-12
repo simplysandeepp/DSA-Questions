041b7d4388da5c308e17a73cd2218bd5772baa17
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-01-12 12:00:53 +0530

    Time: 14 ms (95.66%), Space: 44.5 MB (61.79%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,71 +1,34 @@
 class Solution {
-    static int dp[][];
-    public Integer minCost(int n, int[] cuts) {
-        int c = cuts.length;
-        dp = new int[c+2][c+2];
-        for(int i = c-1 ;i >= 1; i--) {
-            for(int j = i+1; j <= c-1; j ++) {
-                int mini = Integer.MAX_VALUE;
-                for(int idx = i; idx < j; idx++) {
-                    int cost = cuts[i-1] * cuts[idx] * cuts[j] + 
-                    dp[i][idx] + dp[idx+1][j];
-            
-                    mini = Math.min(mini, cost);
-            }
-            dp[i][j]= mini;
+    public int minCost(int n, int[] cuts) {
+        Arrays.sort(cuts);
+        int s=cuts.length;
+        int [] arr= new int [s+2];
+        arr[0]=0;
+        arr[s+1]= n;
+        for(int i =1; i<=s;i++){
+            arr[i]=cuts[i-1];
         }
-    }
-    return dp[1][c-1];
+        int [][] dp = new int [s+2][s+2];
+        for(int i=s; i>=1;i--){
+            for(int j=1; j<=s;j++){
+                if (i>j) continue;
+                int min = Integer.MAX_VALUE;
+                for(int idx=i;idx<=j; idx++){
+                    int cost= arr[j+1]-arr[i-1] +dp[i][idx-1]+dp[idx+1][j];
+                    min= Math.min(min, cost);
+                }dp[i][j]= min;  
+            }
+        // return helper(1,s,arr, dp);
+        }return dp[1][s];
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-//     static Integer dp[][];
-//     public Integer minCost(int n, int[] cuts) {
-//         int m = cuts.length;
-//         int[] arr = new int[m + 2];
-//         arr[0] = 0;
-//         arr[m + 1] = n;
-//         for (int i = 0; i < m; i++) {
-//             arr[i + 1] = cuts[i];
-//         }
-//         Arrays.sort(arr);
-//         dp = new Integer[m + 2][m + 2];
-//         return solve(1, m, arr);
+// public int helper(int i, int j, int [] arr, Integer [][] dp){
+//     int min= (int)1e9;
+//     if (i>j) return 0;
+//     if(dp[i][j]!=null) return dp[i][j];
+//     for(int idx=i;idx<=j; idx++){
+//         int cost= arr[j+1]-arr[i-1] +helper(i, idx-1, arr,dp)+helper(idx+1,j,arr,dp);
+//         min= Math.min(min, cost);
 //     }
-
-//     static int solve(int i, int j, int[] arr) {
-//         if (i > j) {
-//             return 0;
-//         }
-//         if (dp[i][j] != null) {
-//             return dp[i][j];
-//         }
-//         int mini = Integer.MAX_VALUE;
-//         for (int idx = i; idx <= j; idx++) {
-//             int cost = (arr[j+1] - arr[i-1]) +
-//                        solve(i, idx - 1, arr) +
-//                        solve(idx + 1, j, arr);
-//             mini = Math.min(mini, cost);
-//         }
-
-//         return dp[i][j] = mini;
-//     }
+//     return dp[i][j]= min;
 // }
