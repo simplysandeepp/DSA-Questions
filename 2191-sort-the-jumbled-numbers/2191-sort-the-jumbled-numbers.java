0a4cb55521aa4a10b233c44105090d8ed44e8ca8
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-01-24 20:25:07 +0530

    Time: 97 ms (57.14%), Space: 78.2 MB (38.34%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
@@ -1,26 +1,28 @@
 class Solution {
-    public int[] sortJumbled(int[] mapping, int[] nums) {
-
-        int n = nums.length;
-        long[][] arr = new long[n][2];
-
-        for (int i = 0; i < n; i++) {
-            arr[i][0] = map(nums[i], mapping);
-            arr[i][1] = i;
+    class Pair implements Comparable<Pair> {
+        long mapped;
+        int index;
+        Pair(long mapped, int index) {
+            this.mapped = mapped;
+            this.index = index;
         }
-
-        Arrays.sort(arr, (a, b) -> {
-            if (a[0] != b[0]) return Long.compare(a[0], b[0]);
-            return Long.compare(a[1], b[1]);
-        });
-
+        public int compareTo(Pair other) {
+            if (this.mapped != other.mapped)
+                return this.mapped < other.mapped ? -1 : 1;
+            return this.index - other.index;
+        }
+    }
+    public int[] sortJumbled(int[] mapping, int[] nums) {
+        int n = nums.length;
+        Pair[] arr = new Pair[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = new Pair(map(nums[i], mapping), i);
+        Arrays.sort(arr);
         int[] res = new int[n];
         for (int i = 0; i < n; i++)
-            res[i] = nums[(int) arr[i][1]];
-
+            res[i] = nums[arr[i].index];
         return res;
     }
-
     long map(int num, int[] m) {
         if (num == 0) return m[0];
         long val = 0, p = 1;
