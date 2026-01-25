af6c7e247a892e3712799cb0573d57bebdfd244f
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-01-25 21:06:49 +0530

    Time: 2 ms (96.26%), Space: 48.5 MB (51.86%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,17 +3,18 @@ class Solution {
         int left = 0;
         int right = numbers.length - 1;
 
-        while (left < right) {
+        while (left<right) {
             int total = numbers[left] + numbers[right];
 
-            if (total == target) {
-                return new int[]{left + 1, right + 1};
-            } else if (total > target) {
+            if (total==target) {
+                return new int[] {left+1, right+1};
+
+            } else if (total>target) {
                 right--;
-            } else {
+            } else{
                 left++;
             }
         }
-        return new int[]{-1, -1};         
+        return new int[]{-1,-1};         
     }
 }
