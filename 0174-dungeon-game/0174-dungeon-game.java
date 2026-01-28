6e81cecf7f5ae2d0d564e585eeaa6aca4a899b40
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-01-28 17:22:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,45 +1,5 @@
 class Solution {
     int m, n;
-    int[][] dp;
-
-    public int calculateMinimumHP(int[][] power) {
-        m = power.length;
-        n = power[0].length;
-        dp = new int[m][n];
-
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                dp[i][j] = -1;
-
-        return solve(0, 0, power);
-    }
-
-    int solve(int i, int j, int[][] power) {
-        if (i >= m || j >= n) return Integer.MAX_VALUE;
-
-        if (i == m - 1 && j == n - 1) {
-            return power[i][j] >= 0 ? 1 : 1 - power[i][j];
-        }
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int down = solve(i + 1, j, power);
-        int right = solve(i, j + 1, power);
-
-        int need = Math.min(down, right) - power[i][j];
-        dp[i][j] = need <= 0 ? 1 : need;
-
-        return dp[i][j];
-    }
-}
-
-
-
-// Recursive Approach !!
-
-/*
-class Solution {
-    int m, n;
 
     public int calculateMinimumHP(int[][] power) {
         m = power.length;
@@ -62,4 +22,3 @@ class Solution {
         return need <= 0 ? 1 : need;
     }
 }
-*/
