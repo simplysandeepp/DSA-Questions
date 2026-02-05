5da6e61175a92082d36556d7f33415bea9448420
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-02-06 01:46:15 +0530

    Time: 2 ms (100%), Space: 46.4 MB (88.52%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int closedIsland(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int closed = 0;
+        for (int i = 1; i < rows - 1; i++) {
+            for (int j = 1; j < cols - 1; j++) {
+                if (grid[i][j] == 0) {
+                    if (dfs(grid, i, j)) {
+                        closed++;
+                    }
+                }
+            }
+        }
+        return closed;
+    }
+
+    public boolean dfs(int[][] grid, int i, int j) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        if (i < 0 || j < 0 || i >= rows || j >= cols) {
+            return false;
+        }
+        if (grid[i][j] == 1) {
+            return true;
+        }
+        grid[i][j] = 1; 
+        boolean down = dfs(grid, i + 1, j);
+        boolean up = dfs(grid, i - 1, j);
+        boolean right = dfs(grid, i, j + 1);
+        boolean left = dfs(grid, i, j - 1);
+
+    // agar charo option hai dfs k pass traverse krne ka tab return sate me 4 aayega otherwise skip 
+        return down && up && right && left;
+    }
+}
