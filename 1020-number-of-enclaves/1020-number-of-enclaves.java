132f541d91af49fb4fc0b60f38ca4373a099282e
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-02-05 15:12:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for (int i=0;i<rows;i++) {
+            for (int j=0;j<cols;j++) {
+                if ((i==0 || j==0 || i==cols-1 || j==rows-1) && grid[i][j] == 1) {
+                    dfs(grid,i, j);
+                }
+            }
+        }
+        int move = 0;
+        for (int i=0;i<rows;i++) {
+            for (int j=0;j<cols;j++) {
+                move += grid[i][j]; 
+            }
+        }
+        return move;
+    }
+    public void dfs(int[][] grid,int i ,int j){
+        int rows = grid.length;
+        int cols = grid[0].length;
+        if (i<0 || j<0 || i>=rows || j>=cols || grid[i][j]==0) {
+            return;
+        }
+        grid[i][j] = 0;
+        dfs(grid,i+1, j);
+        dfs(grid,i-1, j);
+        dfs(grid,i, j+1);
+        dfs(grid,i, j-1);
+    }
+}
