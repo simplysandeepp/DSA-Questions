c016b3173d1fa89fa7fabfadad3db28f861a49b7
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-02-06 11:24:01 +0530

    Time: 7 ms (16.09%), Space: 51.5 MB (87.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,59 @@
-class Solution {
-    public int numIslands(char[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        int count=0;
+import java.util.*;
 
-        for(int i=0; i<rows; i++) {
-            for(int j=0; j<cols; j++)  {
-                if (grid[i][j] == '1') {
-                    count++;
-                    dfs(grid,i,j);
+class Solution {
+    static int rLen;
+    static int cLen;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
+    static class Pair {
+        int row;
+        int col;
+        Pair(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        rLen = grid.length;
+        cLen = grid[0].length;
+
+        boolean[][] vis = new boolean[rLen][cLen];
+        int components = 0;
+
+        for (int i = 0; i < rLen; i++) {
+            for (int j = 0; j < cLen; j++) {
+                if (!vis[i][j] && grid[i][j] == '1') {
+                    bfs(i, j, vis, grid);
+                    components++;
                 }
             }
         }
-        return count;
+        return components;
     }
-    static void dfs(char[][]grid, int r, int c) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        if (r<0 || c<0 || r>=rows || c>=cols || grid[r][c]=='0') {
-            return;
+
+    public static void bfs(int r, int c, boolean[][] vis, char[][] grid) {
+        Queue<Pair> q = new LinkedList<>();
+        vis[r][c] = true;
+        q.offer(new Pair(r, c));
+
+        while (!q.isEmpty()) {
+            Pair curr = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nRow = curr.row + dr[i];
+                int nCol = curr.col + dc[i];
+
+                if (nRow >= 0 && nCol >= 0 &&
+                    nRow < rLen && nCol < cLen &&
+                    !vis[nRow][nCol] &&
+                    grid[nRow][nCol] == '1') {
+
+                    vis[nRow][nCol] = true;
+                    q.offer(new Pair(nRow, nCol));
+                }
+            }
         }
-        grid[r][c]='0';
-        dfs(grid,r+1,c); // down
-        dfs(grid,r-1,c); // up
-        dfs(grid,r,c+1); // right
-        dfs(grid,r,c-1); // left
     }
 }
