46314a24f3eb446d8e271a442d02140a5d3fd6fc
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-02-16 11:14:37 +0530

    Time: 13 ms (88.09%), Space: 47.1 MB (71.16%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.java
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.java
@@ -1,39 +1,42 @@
 class Solution {
-    public boolean findSafeWalk(final List<List<Integer>> grid, final int health) {
-        final int m = grid.size(), n = grid.get(0).size();
-        final int[][] dirs = new int[][] { 
-            { 0, 1 }, 
-            { 1, 0 }, 
-            { 0, -1 }, { -1, 0 } };
-        final int[][] healths = new int[m][n];
+    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+        int m = grid.size();
+        int n = grid.get(0).size();
 
-        final Queue<int[]> queue = new PriorityQueue<>((a, b) -> b[2] - a[2]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[2] - a[2]);
 
-        queue.offer(new int[] { 0, 0, health - grid.get(0).get(0) });
+        int[][] bh = new int[m][n];
+        for (int[] row : bh) {
+            Arrays.fill(row, -1);
+        }
 
-        while(!queue.isEmpty()) {
-            final int[] curr = queue.poll();
+        int initial = health - grid.get(0).get(0);
+        pq.offer(new int[]{0, 0, initial});
+        bh[0][0] = initial;
 
-            if(curr[2] == 0)
-                continue;
+        int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int x = curr[0];
+            int y = curr[1];
+            int h = curr[2];
 
-            if(curr[0] == m - 1 && curr[1] == n - 1)
+            if (x == m - 1 && y == n - 1 && h > 0) {
                 return true;
+            }
 
-            for(final int[] dir : dirs) {
-                final int newX = curr[0] + dir[0], newY = curr[1] + dir[1];
+            for (int[] d : directions) {
+                int nx = x + d[0];
+                int ny = y + d[1];
 
-                if(newX < 0 || newY < 0 || newX >= m || newY >= n)
-                    continue;
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
 
-                final int newHealth = curr[2] - grid.get(newX).get(newY);
-
-                if(healths[newX][newY] >= newHealth)
-                    continue;
-
-                healths[newX][newY] = newHealth;
-
-                queue.offer(new int[] { newX, newY, newHealth });
+                int nh = h - grid.get(nx).get(ny);
+                if (nh > 0 && nh > bh[nx][ny]) {
+                    bh[nx][ny] = nh;
+                    pq.offer(new int[]{nx, ny, nh});
+                }
             }
         }
 
