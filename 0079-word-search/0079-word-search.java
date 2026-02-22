d37bb84b73dcafa3f609897087bedf188b86024d
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-02-22 20:34:35 +0530

    Time: 132 ms (68.3%), Space: 43.3 MB (20.56%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,25 +1,36 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int n = word.length();
-        StringBuilder arr = new StringBuilder();
-        return helper(1,1,arr); 
-    }
-    public static boolean helper(char i, char j, StringBuilder arr) {
-        if(n==0) return false;
-        if(n==1) {
-            if(board[i][j] == word.charAt(0)) return true;
-            else return false; 
-        }
-        int m=board.length;
-        for(char i; i<m;i++) {
-            for(char j;j<board[0].length(); j++) {
-                if(board[i][j] == word.charAt(0)) {
-                    arr.append(board[i][j]);
-                } 
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
             }
         }
-        if(word.equals(arr.toString())) {
-            return true; 
-        } else return false;
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+        if (index == word.length()) return true;
+
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length ||
+            board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = '#';  
+
+        boolean found = dfs(board, word, i + 1, j, index + 1) ||
+                        dfs(board, word, i - 1, j, index + 1) ||
+                        dfs(board, word, i, j + 1, index + 1) ||
+                        dfs(board, word, i, j - 1, index + 1);
+
+        board[i][j] = temp;  
+
+        return found;
     }
 }
