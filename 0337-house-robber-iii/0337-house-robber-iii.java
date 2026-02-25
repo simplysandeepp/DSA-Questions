86814a6702adc01030b2d1ca2ad2b41c2821b138
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-02-25 14:01:54 +0530

    Time: 0 ms (100%), Space: 46.8 MB (42.39%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(TreeNode root) {
+        int choice[] = helper(root);
+        return Math.max(choice[0], choice[1]);
+    }
+    public int[] helper(TreeNode root) {
+        if(root == null) {
+            return new int[2];
+        }
+        int[] lc = helper(root.left);
+        int[] rc = helper(root.right);
+        int[] choice = new int[2];
+        //rob
+        choice[0] = root.val + lc[1]+rc[1];
+        // notRob
+        choice[1] = Math.max(lc[0],lc[1]) + Math.max(rc[0],rc[1]);
+        return choice;
+    }
+}
