ad257d981f589e23df09f76b901b353c0ddef835
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-03-11 10:28:27 +0530

    Time: 0 ms (100%), Space: 44.5 MB (86.31%) - LeetHub

--- LeetCode/Hard/0968-binary-tree-cameras/0968-binary-tree-cameras.java
+++ LeetCode/Hard/0968-binary-tree-cameras/0968-binary-tree-cameras.java
@@ -16,22 +16,18 @@
 class Solution {
     private int cameras = 0;
     public int minCameraCover(TreeNode root) {
-        return dfs(root) == -1 ? cameras + 1 : cameras;
+        return helper(root) == -1 ? cameras + 1 : cameras;
     }
-    private int dfs(TreeNode root) {
-        if (root == null) return 0;
-        
-        int left = dfs(root.left);
-        int right = dfs(root.right);
-        
+    private int helper(TreeNode root) {
+        if (root==null) return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
         if (left == -1 || right == -1) {
             cameras++;
             return 1; 
         }
-        
         if (left == 1 || right == 1)
             return 0; 
-        
         return -1;
     }
 }
